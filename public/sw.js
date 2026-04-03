269dd07b397d1569074c33e93c1d1aa012bc2638
rachwood1111 <rachwood1111@gmail.com>
2026-04-03 18:50:20 +0800

    Update service worker cache version and improve code

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,27 @@
-// Basic Service Worker for PWA support
-const CACHE_NAME = 'ttm-wa-v2';
+// Service Worker for PWA support
+const CACHE_NAME = 'ttm-wa-v7';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
     })
   );
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
